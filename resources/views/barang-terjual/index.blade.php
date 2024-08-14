44758b7f25ed8438de3b2a6bce17e2cab59355d8
ratuufaradiiba <ratuufaradyb.05@gmail.com>
2024-08-14 13:58:35 +0700

    Feat #18 - Rapihin Table Barang Terjual

--- resources/views/barang-terjual/index.blade.php
+++ resources/views/barang-terjual/index.blade.php
@@ -6,7 +6,58 @@
             <div>
                 <h1 class="my-4">Barang Terjual</h1>
             </div>
-            <div class="card">
+            <div class="row">
+                <div class="col-6">
+                    <div class="card h-100">
+                        <div class="card-header">
+                            <h4 class="card-title">Multi Filter Select</h4>
+                        </div>
+                        <div class="card-body" style="max-height: 400px; overflow-y: auto;">
+                            <div class="table-responsive">
+                                <table id="multi-filter-select" class="display table table-striped table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>NO</th>
+                                            <th>Nama Barang</th>
+                                            <th>Stok Masuk</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($barangTerjualGroupBy as $key => $data)
+                                            <tr>
+                                                <td>{{ $key + 1 }}</td>
+                                                <td>{{ $data->katalogBarang->nama_barang }}</td>
+                                                <td> {{ $data->total_jumlah_terjual }}</td>
+                                                <td>
+                                                    <a href="" class="btn btn-info mx-1" style="font-size: 16px;">
+                                                        <i class="icon-eye" style="font-size: 16px;"></i> Detail Data
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-6">
+                    <div class="card h-100">
+                        <div class="card-header">
+                            <div class="card-title">Line Chart</div>
+                        </div>
+                        <div class="card-body">
+                            <div class="chart-container">
+                                <canvas id="lineChart"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mt-5">
                 <div class="card-header">
                     <h4 class="card-title">Multi Filter Select</h4>
                 </div>
@@ -56,4 +107,82 @@
             </div>
         </div>
     </div>
+    <script src="{{ asset('assets/js/core/jquery-3.7.1.min.js') }}"></script>
+    <script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
+    <!-- Chart JS -->
+    <script src="{{ asset('assets/js/plugin/chart.js/chart.min.js') }}"></script>
+    <!-- jQuery Scrollbar -->
+    <script src="{{ asset('assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js') }}"></script>
+    <!-- Kaiadmin JS -->
+    <script src="{{ asset('assets/js/kaiadmin.min.js') }}"></script>
+    <!-- Kaiadmin DEMO methods, don't include it in your project! -->
+    <script src="{{ asset('assets/js/setting-demo2.js') }}"></script>
+    <script>
+        var lineChart = document.getElementById("lineChart").getContext("2d")
+
+        var myLineChart = new Chart(lineChart, {
+            type: "line",
+            data: {
+                labels: [
+                    "Jan",
+                    "Feb",
+                    "Mar",
+                    "Apr",
+                    "May",
+                    "Jun",
+                    "Jul",
+                    "Aug",
+                    "Sep",
+                    "Oct",
+                    "Nov",
+                    "Dec",
+                ],
+                datasets: [{
+                    label: "Active Users",
+                    borderColor: "#1d7af3",
+                    pointBorderColor: "#FFF",
+                    pointBackgroundColor: "#1d7af3",
+                    pointBorderWidth: 2,
+                    pointHoverRadius: 4,
+                    pointHoverBorderWidth: 1,
+                    pointRadius: 4,
+                    backgroundColor: "transparent",
+                    fill: true,
+                    borderWidth: 2,
+                    data: [
+                        542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 900,
+                    ],
+                }, ],
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                legend: {
+                    position: "bottom",
+                    labels: {
+                        padding: 10,
+                        fontColor: "#1d7af3",
+                    },
+                },
+                tooltips: {
+                    bodySpacing: 4,
+                    mode: "nearest",
+                    intersect: 0,
+                    position: "nearest",
+                    xPadding: 10,
+                    yPadding: 10,
+                    caretPadding: 10,
+                },
+                layout: {
+                    padding: {
+                        left: 15,
+                        right: 15,
+                        top: 15,
+                        bottom: 15
+                    },
+                },
+            },
+        });
+    </script>
 @endsection
