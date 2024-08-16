daa926d0bebaa4fc9de977c64ec18b3e7c87d603
ratuufaradiiba <ratuufaradyb.05@gmail.com>
2024-08-16 17:28:39 +0700

    feat:
    Create Insert Barang Terjual
    
    Add Store on Controller, button create and Add Form Create Barang Terjual from ajax

--- app/Http/Controllers/BarangTerjualController.php
+++ app/Http/Controllers/BarangTerjualController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\BarangTerjual;
+use App\Models\Katalogbarang;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class BarangTerjualController extends Controller
 {
@@ -13,9 +15,40 @@ class BarangTerjualController extends Controller
     public function index()
     {
         $barangTerjual = BarangTerjual::with('katalogBarang')->get();
+        $katalogBarang = Katalogbarang::all();
         $barangTerjualGroupBy = BarangTerjual::getBarangTerjualByGroup();
 
-        return view('barang-terjual.index', compact('barangTerjual', 'barangTerjualGroupBy'));
+        return view('barang-terjual.index', compact('barangTerjual', 'barangTerjualGroupBy', 'katalogBarang'));
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'katalog_barang_id' => 'required|integer',
+            'jumlah_terjual' => 'required|integer',
+            'keterangan' => 'required|string',
+        ]);
+
+        try {
+            $barangTerjual = BarangTerjual::create($validated);
+
+            if (!$barangTerjual) {
+                throw new \Exception('Gagal memuat data barang terjual.');
+            }
+
+            return response()->json(['success' => 'Data Berhasil Ditambahkan']);
+        } catch (\Exception $e) {
+            Log::error('Error storing barang terjual:' . $e->getMessage());
+            return response()->json(['error' => 'Terjadi Kesalahan:' . $e->getMessage()], 500);
+        }
     }
 
     public function showDetail($katalog_barang_id)
@@ -28,33 +61,11 @@ class BarangTerjualController extends Controller
         return view('barang-terjual.detail', compact('detailBarangTerjual', 'namaBarang'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         //
