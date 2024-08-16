8358ad0ff376abc43084decf40e0bad55a5e89be
ratuufaradiiba <ratuufaradyb.05@gmail.com>
2024-08-17 00:31:52 +0700

    feat: CREATE Insert Barang Terjual by Kategori

--- resources/views/barang-terjual/detail.blade.php
+++ resources/views/barang-terjual/detail.blade.php
@@ -20,7 +20,8 @@
                             </div>
                             Kembali
                         </a>
-                        <a href="#" class="btn btn-label-primary btn-round btn-md">
+                        <a href="#" class="btn btn-label-primary btn-round btn-md" type="button"
+                            id="modalCreateByKategori"">
                             <div class="btn-label">
                                 <i class="fas fa-plus"></i>
                             </div>
@@ -69,4 +70,88 @@
             </div>
         </div>
     </div>
+    <script src="{{ asset('assets/js/core/jquery-3.7.1.min.js') }}"></script>
+    <script>
+        $("#modalCreateByKategori").click(function(e) {
+            var selectedBarangId = @json($katalog_barang_id);
+            var selectedNamaBarang = "{{ $namaBarang }}";
+
+            swal({
+                title: `Tambah Data ${selectedNamaBarang} Terjual`,
+                content: {
+                    element: "div",
+                    attributes: {
+                        innerHTML: `
+                        <div class="form-group">
+                            <label for="nama-barang">Nama Barang</label>
+                            <input type="text" class="form-control" id="nama-barang" value="${selectedNamaBarang}" readonly>
+                            <input type="hidden" id="katalog-barang-id" value="${selectedBarangId}">
+                        </div>
+                        <div class="form-group mt-2">
+                            <label for="jumlah-terjual">Terjual</label>
+                            <input min="1" type="number" class="form-control" id="jumlah-terjual" placeholder="Jumlah Stok Terjual" name="jumlah_terjual">
+                        </div>
+                        <div class="form-group mt-2">
+                            <label for="keterangan">Keterangan</label>
+                            <input type="text" class="form-control" id="keterangan" placeholder="Keterangan" name="keterangan">
+                        </div>`
+                    }
+                },
+                buttons: {
+                    cancel: {
+                        visible: true,
+                        className: "btn btn-danger",
+                    },
+                    confirm: {
+                        text: "Simpan",
+                        className: "btn btn-success",
+                    },
+                },
+            }).then(function(result) {
+                if (result) {
+                    let katalogBarangId = $("#katalog-barang-id").val();
+                    let jumlahTerjual = $("#jumlah-terjual").val();
+                    let keterangan = $("#keterangan").val();
+
+                    $.ajax({
+                        type: 'POST',
+                        url: '{{ route('barang-terjual.store') }}',
+                        data: {
+                            katalog_barang_id: katalogBarangId,
+                            jumlah_terjual: jumlahTerjual,
+                            keterangan: keterangan,
+                            _token: '{{ csrf_token() }}',
+                        },
+                        success: function(response) {
+                            swal("Sukses!", "Data telah Ditambahkan!", "success");
+                            setTimeout(function() {
+                                location.reload();
+                            }, 2000);
+                        },
+                        error: function(xhr, status, error) {
+                            var errors = xhr.responseJSON.errors;
+                            var errorMessages = '';
+
+                            $.each(errors, function(key, messages) {
+                                messages.forEach(function(message) {
+                                    errorMessages += message + '<br>';
+                                });
+                            });
+
+                            swal({
+                                title: "Error!",
+                                content: {
+                                    element: "div",
+                                    attributes: {
+                                        innerHTML: `<div>${errorMessages}</div>`
+                                    }
+                                },
+                                icon: "error",
+                            });
+                        }
+                    });
+                }
+            });
+        });
+    </script>
 @endsection
