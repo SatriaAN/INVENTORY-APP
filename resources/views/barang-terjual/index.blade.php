cf6b7fa53fc65f98d1a119b47df123255d97e7dc
ratuufaradiiba <ratuufaradyb.05@gmail.com>
2024-08-22 17:02:37 +0700

    feat: Make New Feat  Sorting, Pagination, dan Filtering on view detail and index Barang Terjual

--- resources/views/barang-terjual/index.blade.php
+++ resources/views/barang-terjual/index.blade.php
@@ -14,7 +14,7 @@
                         </div>
                         <div class="card-body" style="max-height: 335px; overflow-y: auto;">
                             <div class="table-responsive">
-                                <table id="multi-filter-select" class="display table table-striped table-hover">
+                                <table class="display table table-striped table-hover">
                                     <thead>
                                         <tr>
                                             <th>NO</th>
@@ -442,6 +442,48 @@
                         },
                     });
                 }
+            });
+        });
+    </script>
+    <script>
+        $(document).ready(function() {
+            var table = $("#multi-filter-select").DataTable({
+                pageLength: 7,
+                columnsDefsc: [{
+                    targets: 2,
+                    type: 'num',
+                    render: function(data, type, row) {
+                        var num = parseInt(data.split(' - ')[0], 10);
+                        if (type === 'sort' || type === 'type') {
+                            return num;
+                        }
+                        return num + ' - STOK';
+                    }
+                }],
+                initComplete: function() {
+                    this.api().columns([1, 2, 3]).every(function() {
+                        var column = this;
+                        var select = $(
+                                '<select class="form-select"><option value=""></option></select>'
+                            )
+                            .appendTo($(column.footer()).empty())
+                            .on("change", function() {
+                                var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                                column.search(val ? "^" + val + "$" : "", true, false)
+                                    .draw();
+                            });
+
+                        column.data().unique().sort().each(function(d, j) {
+                            var cleanedValue = d.split(' - ')[0] + ' - STOK';
+                            select.append('<option value="' + cleanedValue + '">' +
+                                cleanedValue + "</option>");
+                        });
+                    });
+                },
+            });
+
+            $('#multi-filter-select_filter input').on('keyup change', function() {
+                table.draw();
             });
         });
     </script>
